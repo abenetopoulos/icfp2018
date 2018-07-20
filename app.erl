8a784d9656c5a6191b2d14b0566d38ed845a8f68
Achilles Benetopoulos <abenetopoulos@gmail.com>
2018-07-20 21:40:00 +0300

    Implement trace file output

--- app.erl
+++ app.erl
@@ -4,4 +4,7 @@
 
 main() ->
     Model = parse_mdl:parse("problemsL/LA001_tgt.mdl"),
-    io:format("Model:~n~p~n", [Model]).
+    io:format("Model:~n~p~n", [Model]),
+    output:write_trace_file([{halt, []}, {wait, []}, {flip, []}, {smove, [{12, 0, 0}]}, {lmove, [{0, 2, 0}, {-1, 0, 0}]},
+                             {fusionp, [{1,2,3}]}, {fusions, [{1,2,3}]}, {fission, [{1,2,3}, 42]}, {fill, [{1,2,3}]}]),
+    io:format("Trace written~n").
